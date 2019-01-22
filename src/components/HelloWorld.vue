f3bd882d4deb73f2103e3d8f2524c50d00c31e9a
mengday.zhang <mengday.zhang@ad-juster.com>
2019-01-22 22:26:10 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <el-button type="primary">主要按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
